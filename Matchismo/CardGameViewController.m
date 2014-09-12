746322c12fffcd29a8d2391a67e1bca770adf0c8
Chao Huang <chaoh@glare8.cs.clemson.edu>
2014-09-12 09:31:55 -0400

    now the card can disappear after appear with in 1 second.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,18 +14,28 @@
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UILabel *SamePostionLabel;
-//@property (nonatomic) int flipCount;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSMutableArray *cardButtons;
 @property (strong,nonatomic) Deck *deck;
 @property (strong,nonatomic) CardMatchingGame *game;
+@property (weak, nonatomic) IBOutlet UITextView *CardCount;
+@property (weak, nonatomic) IBOutlet UILabel *IntroductionLabel;
+
+
 
 @property (nonatomic) NSUInteger currentPos;
-//@property (nonatomic) NSUInteger samepos;
+@property (nonatomic) NSUInteger stepCount;
+@property (nonatomic) NSUInteger CardCountNumber;
 
 
 @end
 
 @implementation CardGameViewController
+
+- (void) viewDidLoad
+{
+    self.CardCountNumber = 9;
+}
+
 
 - (CardMatchingGame *)game
 {
@@ -47,14 +57,10 @@
     return [[PlayingCardDeck alloc] init];
 }
 
-/*
-- (void) setFlipCount:(int)flipCount{
-    _flipCount = flipCount;
-    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d",self.flipCount];
-    NSLog(@"HELLO = %d",self.flipCount);
+- (IBAction)SetCardCount:(UIButton *)sender {
+    self.CardCountNumber = [self.CardCount.text intValue] - 1;
+    self.IntroductionLabel.text = [NSString stringWithFormat:@"You have selected %d cards in this game",self.CardCountNumber+1];
 }
-*/
-
 
 - (IBAction)touchCardButton:(UIButton *)sender
 {
@@ -68,34 +74,17 @@
         [self.game.existCard addObject:newcard];
 
         int samepos = [self.game checkStatus:self.game.existCard];//check same position
-
-        
-        //self.scoreLabel.text = [NSString stringWithFormat:@"existnumber: %d, sameposition: %d",self.game.existCard.count,samepos];
-        self.SamePostionLabel.text = [NSString stringWithFormat:@"Same Position is: %d",samepos];
-
-        
-        self.currentPos++;
-        
-        NSDictionary *userinfo = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:samepos], @"samepos",nil];
-        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(RefreshTable:) userInfo:userinfo repeats:NO];
         
         [self viewWillAppear:YES];
         
-        /*
-         if ([sender.currentTitle length]){
-            UIImage *cardImage = [UIImage imageNamed:@"cardback"];
-            [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-            [sender setTitle:@"" forState:UIControlStateNormal];
-         }
-         else{
-            Card *randomCard = [self.deck drawRandCard];
-            if (randomCard) {
-                UIImage *cardImage = [UIImage imageNamed:@"cardfront"];
-                [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-                [sender setTitle:randomCard.contents forState:UIControlStateNormal];
-            }
-         }
-         */
+        self.stepCount++;
+        self.SamePostionLabel.text = [NSString stringWithFormat:@"Step Cost is %d",self.stepCount];
+        self.currentPos++;
+        
+        NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:samepos], @"samepos", nil];
+        
+        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(RefreshTable:) userInfo:userInfo repeats:NO];
+        [self viewWillAppear:YES];
     }
 }
 
@@ -115,32 +104,18 @@
             [self.game.cards replaceObjectAtIndex:i withObject:randCard];
         }
     }
-    
-    if (self.game.existCard.count == 0) {
-        self.scoreLabel.text = [NSString stringWithFormat:@"You win!"];
-    }
+    if (self.game.existCard.count == self.CardCountNumber+1)
+        self.scoreLabel.text = [NSString stringWithFormat:@"Game Over!"];
     else{
-        self.scoreLabel.text = [NSString stringWithFormat:@"Exist Card: %d",self.currentPos];
+        if (self.game.existCard.count == 0) {
+            self.stepCount = 0;
+            self.scoreLabel.text = [NSString stringWithFormat:@"You win!"];
+        }
+        else{
+            self.scoreLabel.text = [NSString stringWithFormat:@"Exist Card: %d",self.currentPos];
+        }
     }
-    
     [self viewWillAppear:YES];
-    
-}
-
-- (void) myPause
-{
-    //[NSThread sleepForTimeInterval:1.0];
-    /*
-    double delayInSeconds = 2.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){NSLog(@"DO SOME WORK");});
-     */
-    int m=0;
-    for (int i = 0;i<100000000;i++)
-    {
-        m++;
-        m--;
-    }
 }
 
 - (void) viewWillAppear:(BOOL)animated
@@ -149,37 +124,21 @@
     {
         int buttonIndex = [self.cardButtons indexOfObject:cardButton];
         Card *card = [self.game cardAtIndex:buttonIndex];
-        if (buttonIndex<self.currentPos) {
+        if (buttonIndex<self.currentPos)
             card.Chosen = YES;
-        }
-        else{
+        else
             card.Chosen = NO;
-        }
-        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
-        [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState: UIControlStateNormal];
-    }
-}
-
-- (void) updateUI
-{
-    for (UIButton *cardButton in self.cardButtons)
-    {
-        int buttonIndex = [self.cardButtons indexOfObject:cardButton];
-        Card *card = [self.game cardAtIndex:buttonIndex];
-        if (buttonIndex<self.currentPos) {
-            card.Chosen = YES;
-        }
-        else{
-            card.Chosen = NO;
-        }
+        
+        if (self.CardCountNumber < self.currentPos)
+            card.matched = YES;
+        else
+            card.matched = NO;
         
         [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
         [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState: UIControlStateNormal];
-        
-        //cardButton.enabled = !card.isMatched;//set whether the card should be put as ismatched
+        cardButton.enabled = !card.isMatched;
     }
 }
-
 
 - (NSString *)titleForCard:(Card *)card
 {
